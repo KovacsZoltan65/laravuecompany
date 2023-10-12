7ead34f00a566072af8393d760e7f6e1ef19c354
Kovács Zoltán <kovacs.zoltan@softc.hu>
2023-10-12 07:42:56 +0200

    Employees

--- resources/js/Pages/Employees/Index.vue
+++ resources/js/Pages/Employees/Index.vue
@@ -59,6 +59,24 @@
         form.reset();
     };
 
+    const save = () => {
+        if( operation.value == 1 ){
+            form.post(route('employees.store'), {
+                onSuccess: () => { ok('Employee created') }
+            });
+        }else{
+            form.put(route('employees.update', id.value), {
+                onSuccess: () => { ok('Employee updated') }
+            });
+        }
+    };
+
+    const ok = (message) => {
+        form.reset();
+        closeModal();
+        Swal.fire({title: message, icon:'success'});
+    };
+
     const deleteEmployee = (id, name) => {
         const alerta = Swal.mixin({
             buttonsStyling: true
@@ -72,7 +90,9 @@
         })
         .then((result) => {
             if(result.isConfirmed){
-                form.delete(route('employees.destroy', id));
+                form.delete(route('employees.destroy', id), {
+                    onSuccess: () => { ok('Employee deleted') }
+                });
             }
         });
     };
@@ -90,36 +110,46 @@
 
             <div class="bg-white grid v-screen place-items-center">
                 <div class="mt-3 mb-3 flex">
-                    <Link :href="route('employees.create')"
+                    <PrimaryButton @click="openModal(1)">
+                        <i class="fa-solid fa-plus-circle"></i> Add New
+                    </PrimaryButton>
+                    <!--<Link :href="route('employees.create')"
                           :class="'px-4 py-2 bg-gray-800 text-white border rounded-md font-semibold text-xs'">
                         <i class="fa-solid fa-plus-circle"></i> Add New
-                    </Link>
+                    </Link>-->
                 </div>
             </div>
 
-            <div class="bg-white grid v-screen place-items-center">
+            <div class="bg-white grid v-screen place-items-center overflow-x-auto">
                 <table class="table-auto border border-gray-400">
                     <thead>
                         <tr class="bg-gray-100">
-                            <th class="px-4 py-4">#</th>
-                            <th class="px-4 py-4">NAME</th>
-                            <th class="px-4 py-4">EMAIL</th>
-                            <th class="px-4 py-4"></th>
-                            <th class="px-4 py-4"></th>
+                            <th class="px-2 py-2">#</th>
+                            <th class="px-2 py-2">NAME</th>
+                            <th class="px-2 py-2">EMAIL</th>
+                            <th class="px-2 py-2">PHONE</th>
+                            <th class="px-2 py-2">DEPARTMENT</th>
+                            <th class="px-2 py-2"></th>
+                            <th class="px-2 py-2"></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="emp, i in employees" :key="emp.id">
-                            <td class="border border-gray-400 px-4 py-4">{{ (i+1) }}</td>
-                            <td class="border border-gray-400 px-4 py-4">{{ emp.name }}</td>
-                            <td class="border border-gray-400 px-4 py-4">{{ emp.email }}</td>
-                            <td class="border border-gray-400 px-4 py-4">
-                                <Link :href="route('employees.edit', emp.id)"
+                        <tr v-for="emp, i in employees.data" :key="emp.id">
+                            <td class="border border-gray-400 px-2 py-2">{{ (i+1) }}</td>
+                            <td class="border border-gray-400 px-2 py-2">{{ emp.name }}</td>
+                            <td class="border border-gray-400 px-2 py-2">{{ emp.email }}</td>
+                            <td class="border border-gray-400 px-2 py-2">{{ emp.phone }}</td>
+                            <td class="border border-gray-400 px-2 py-2">{{ emp.email }}</td>
+                            <td class="border border-gray-400 px-2 py-2">
+                                <WarningButton @click="openModal(2, emp.name, emp.email, emp.phone, emp.department_id, emp.id)">
+                                    <i class="fa-solid fa-edit"></i>
+                                </WarningButton>
+                                <!--<Link :href="route('employees.edit', emp.id)"
                                       :class="'px-4 py-2 bg-yellow-400 text-white border rounded-md font-semibold text-xs'">
                                     <i class="fa-solid fa-edit"></i>
-                                </Link>
+                                </Link>-->
                             </td>
-                            <td class="border border-gray-400 px-4 py-4">
+                            <td class="border border-gray-400 px-2 py-2">
                                 <DangerButton @click="$event => deleteEmployee(emp.id, emp.name)">
                                     <i class="fa-solid fa-trash"></i>
                                 </DangerButton>
@@ -127,6 +157,14 @@
                         </tr>
                     </tbody>
                 </table>
+            </div>
+            <div class="bg-white grid v-screen place-items-center">
+                <VueTailwindPagination 
+                    :current="employees.currentPage" 
+                    :total="employees.total"
+                    :perPage="employees.perPage" 
+                    @pageChanged="$event => onPageClick($event)"
+                    ></VueTailwindPagination>
             </div>
         </div>
     </AuthenticatedLayout>
